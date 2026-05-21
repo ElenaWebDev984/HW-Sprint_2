5c753590c0f1b9b1c93bc8d5850f4adff2d65553
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-05-21 16:31:49 +0200

    completed HW4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,10 +18,14 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled
+            ? ' ' + s.disabled
+            : xType === 'red'
+                ? ' ' + s.red
+                : xType === 'secondary'
+                    ? ' ' + s.secondary
+                    : '')
+        + (className ? ' ' + className : '')
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
